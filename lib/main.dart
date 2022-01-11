5caf0b936bf4ae06c4c5950144e3681a087321ad
Allan Muturi <amuturi01@gmail.com>
2022-01-11 22:20:37 +0300

    change hive configs

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:todoapp/models/todo.dart';
 import 'package:todoapp/screen/homepage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path;
+// import 'package:hive/hive.dart';
+// import 'package:path_provider/path_provider.dart' as path;
 
 void main() async {
-  final appDocumentDir = await path.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDir.path);
+  // final appDocumentDir = await path.getApplicationDocumentsDirectory();
+  Hive.initFlutter();
   Hive.registerAdapter(TodoAdapter());
   runApp(const MyApp());
 }
